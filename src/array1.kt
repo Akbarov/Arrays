af8dd3dd74073b91726a648597cffc8e6cd5ed35
zohid <zohdijonakbarov@gmail.com>
2021-03-24 22:18:00 +0500

    first comment

---
+++ src/array1.kt
@@ -0,0 +1,12 @@
+import kotlin.math.pow
+
+fun main() {
+    var twoarray = arrayOf(arrayListOf(1, 2, 3, 4, 4), arrayListOf(3, 4, 5, 5, 6))
+    var arrays = Array(10){IntArray(20)}
+    for (row in twoarray) {
+        println(row.joinToString(prefix = "{",postfix = "}"))
+    }
+    for (row in arrays) {
+        println(row.contentToString())
+    }
+}
